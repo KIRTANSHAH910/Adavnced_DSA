50a37a5cdcdb6027f000acf862ac5d31a4c81d31
KIRTANSHAH910 <shahkirtan2004@gmail.com>
2026-06-03 10:13:23 +0530

    Add reverse number program

---
+++ Striver/Reverse_The_Number.cpp
@@ -0,0 +1,34 @@
+//1. REVERSE THE NUMBER:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n = 12345;
+    int temp = n;
+    int count = 0;
+
+    
+    while(n > 0){
+        n = n / 10;
+        count = count + 1;
+    }
+
+    cout << count << endl;
+
+    int arr[10];
+
+    n = temp;
+
+    for(int i = 0; i < count; i++){
+        arr[i] = n % 10;
+        n = n / 10;
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+
+
