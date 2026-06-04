e016c77eebe4bd96303462cf2242f8036c11c18a
KIRTANSHAH910 <shahkirtan2004@gmail.com>
2026-06-04 09:38:27 +0530

    Add/update completed code files

---
+++ Striver/Reverse_a_given_Array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n=4;
+    int arr[10]={1, 2, 3, 4, 5}; 
+    for(int i=4; i>=0; i--){
+        cout<<" "<< arr[i];
+    }
+    return 0;
+}
