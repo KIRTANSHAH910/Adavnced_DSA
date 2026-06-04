2f8a5443cb4bfa7472f10ec3634dd6dbe1fd61b8
KIRTANSHAH910 <shahkirtan2004@gmail.com>
2026-06-04 09:11:50 +0530

    Add Sum_of_first_N_Natural_Numbers.cpp

---
+++ Striver/Sum_of_first_N_Natural_Numbers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n=5;
+    int sum=0;
+    for(int i=0; i<=n; i++){
+        sum=sum+i;
+        
+        
+    }
+    cout<< " "<< sum<< endl;
+    return 0;
+}
