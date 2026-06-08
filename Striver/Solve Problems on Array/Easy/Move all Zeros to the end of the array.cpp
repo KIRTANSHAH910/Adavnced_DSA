8c0577596aa2212b42167e33eddbb4111cbbf936
KIRTANSHAH910 <shahkirtan2004@gmail.com>
2026-06-08 12:25:47 +0530

    Add next DSA problem solution

---
+++ Striver/Solve Problems on Array/Easy/Move all Zeros to the end of the array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+         vector<int> temp(n);
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                temp[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+       
+    }
+};
